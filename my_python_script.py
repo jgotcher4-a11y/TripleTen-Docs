f02898fcb837843e5c25358e90d1ae7410e4066b
Jason Gotcher <jgotcher4@gmail.com>
2026-04-11 08:21:17 -0400

    Add random greeting generator function

--- my_python_script.py
+++ my_python_script.py
@@ -1,2 +1,19 @@
 print("My first python script in VS Code")
 print("Fly you fools!")
+import random
+
+greetings = [
+    "Hello, Git!",
+    "Greetings, developer!",
+    "Welcome to branching!",
+    "Hi there, coding friend!",
+    "Happy coding!",
+]
+
+
+def get_random_greeting():
+    return random.choice(greetings)
+
+
+print(get_random_greeting())
+print("Learning about branches today!")
